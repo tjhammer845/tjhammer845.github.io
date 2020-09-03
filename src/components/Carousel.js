ef255e6b7733689def002e2aea917aa136450c52
Thomas Hammer <tjhammer845@gmail.com>
2020-09-02 23:00:01 -0400

    carousel code

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,83 +1,83 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../components/Card';
+import Card from "../components/Card";
 
-import devgrub from '../assets/images/devgrub.png';
-import youtube from '../assets/images/youtube.png';
-import evverest from '../assets/images/evverest.png';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import devgrub from "../assets/images/devgrub.png";
+import youtube from "../assets/images/youtube.png";
+import evverest from "../assets/images/evverest.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 class Carousel extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [
-                {
-                    id: 0,
-                    title: 'Dev Grub',
-                    subTitle: 'The cookbook for developers',
-                    imgSrc: devgrub,
-                    link: 'https://devgrub.com',
-                    selected: false
-                },
-                {
-                    id: 1,
-                    title: 'Tom Hammer',
-                    subTitle: 'YouTube channel',
-                    imgSrc: youtube,
-                    link: 'https://www.youtube.com/channel/UCxSITxL2JbF229OGCqieVZw',
-                    selected: false
-                },
-                {
-                    id: 2,
-                    title: 'Evverest',
-                    subTitle: 'A social network for developers',
-                    imgSrc: evverest,
-                    link: 'https://github.com/Tom Hammerlove8/evverest',
-                    selected: false
-                },
-            ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Dev Grub",
+          subTitle: "The cookbook for developers",
+          imgSrc: devgrub,
+          link: "https://d.com",
+          selected: false
+        },
+        {
+          id: 1,
+          title: "Tom Hammer",
+          subTitle: "YouTube channel",
+          imgSrc: youtube,
+          link: "https://www.youtube.com",
+          selected: false
+        },
+        {
+          id: 2,
+          title: "Evverest",
+          subTitle: "A social network for developers",
+          imgSrc: evverest,
+          link: "https://github.com",
+          selected: false
         }
-    }
+      ]
+    };
+  }
 
+  handleCardClick = (id, card) => {
+    let items = [...this.state.items];
 
-    handleCardClick = (id, card) => {
+    items[id].selected = items[id].selected ? false : true;
 
-        let items = [...this.state.items];
+    items.forEach(item => {
+      if (item.id !== id) {
+        item.selected = false;
+      }
+    });
 
-        items[id].selected = items[id].selected ? false : true;
+    this.setState({
+      items
+    });
+  };
 
-        items.forEach(item => {
-            if(item.id !== id) {
-                item.selected = false;
-            }
-        });
+  makeItems = items => {
+    return items.map(item => {
+      return (
+        <Card
+          item={item}
+          click={e => this.handleCardClick(item.id, e)}
+          key={item.id}
+        />
+      );
+    });
+  };
 
-        this.setState({
-            items
-        });
-    }
-
-
-    makeItems = (items) => {
-        return items.map(item => {
-            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
-        })
-    }
-
-
-    render() {
-        return(
-            <Container fluid={true}>
-                <Row className="justify-content-around">
-                    {this.makeItems(this.state.items)}
-                </Row>
-            </Container>
-        );
-    }
-
+  render() {
+    return (
+      <Container fluid={true}>
+        <Row className="justify-content-around">
+          {this.makeItems(this.state.items)}
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Carousel;
