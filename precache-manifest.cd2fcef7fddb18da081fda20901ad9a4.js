dd27945292730badb1503f60549af56f0c6513b3
Thomas Hammer <tjhammer845@gmail.com>
2020-10-08 19:56:45 -0400

    Updates

---
+++ precache-manifest.cd2fcef7fddb18da081fda20901ad9a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00532b4e53f7c7098b27b45da8f6ad0",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "130af5b1e2901ddf71ef",
+    "url": "/portfolio-site/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "3b3ee00abe27c7807286",
+    "url": "/portfolio-site/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "130af5b1e2901ddf71ef",
+    "url": "/portfolio-site/static/js/2.a1e858b2.chunk.js"
+  },
+  {
+    "revision": "3b3ee00abe27c7807286",
+    "url": "/portfolio-site/static/js/main.36bc5c3e.chunk.js"
+  },
+  {
+    "revision": "ea85618c4c8be9d2492a",
+    "url": "/portfolio-site/static/js/runtime~main.7180bc28.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/portfolio-site/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/portfolio-site/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "0f42315392f46e7b84119d08949ca303",
+    "url": "/portfolio-site/static/media/youtube.0f423153.png"
+  }
+]);
