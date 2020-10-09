6f387a10ac23649c9d0000deeb741fc494a255b3
Thomas Hammer <tjhammer845@gmail.com>
2020-10-08 20:59:24 -0400

    react

---
+++ build/precache-manifest.b6a9e1c61c9c55b07774eb2fe53616b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b47de7f9bf6b5a918bcb740e3259c11",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c95c30ba34e8cf3faae",
+    "url": "./static/css/2.207c7e13.chunk.css"
+  },
+  {
+    "revision": "79ab2284e61c4dfa89ac",
+    "url": "./static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "9c95c30ba34e8cf3faae",
+    "url": "./static/js/2.a35f245f.chunk.js"
+  },
+  {
+    "revision": "79ab2284e61c4dfa89ac",
+    "url": "./static/js/main.49effecb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "./static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "./static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "0f42315392f46e7b84119d08949ca303",
+    "url": "./static/media/youtube.0f423153.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa0b6c50a3e44e2bf83bb69f5c2b4f97.js"
+  "./precache-manifest.b6a9e1c61c9c55b07774eb2fe53616b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
