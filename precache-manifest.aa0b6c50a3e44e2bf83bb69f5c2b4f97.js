a9bfcfc0c32a74ef10cdc3275209806380d117f7
Thomas Hammer <tjhammer845@gmail.com>
2020-10-08 20:10:31 -0400

    Updates

---
+++ precache-manifest.aa0b6c50a3e44e2bf83bb69f5c2b4f97.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8e1c5fc8d192dafa0db27fbe77631c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c95c30ba34e8cf3faae",
+    "url": "/static/css/2.207c7e13.chunk.css"
+  },
+  {
+    "revision": "d1de7b1033f4a373a421",
+    "url": "/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "9c95c30ba34e8cf3faae",
+    "url": "/static/js/2.a35f245f.chunk.js"
+  },
+  {
+    "revision": "d1de7b1033f4a373a421",
+    "url": "/static/js/main.5e7bcef3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "0f42315392f46e7b84119d08949ca303",
+    "url": "/static/media/youtube.0f423153.png"
+  }
+]);
