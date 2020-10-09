1afdc17006461fcb67a4c518feae158e6b14d180
Thomas Hammer <tjhammer845@gmail.com>
2020-10-08 21:20:55 -0400

    react

--- src/App.js
+++ src/App.js
@@ -53,11 +53,11 @@ class App extends React.Component {
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
-
                     <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
                     <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
                     <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
 
+                    <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />
                     <Footer />
 
                 </Container>
