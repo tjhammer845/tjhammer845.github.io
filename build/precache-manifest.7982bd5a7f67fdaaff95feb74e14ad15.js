b033fd4fcd8098d202e520be6c50aff4d080d4bf
Thomas Hammer <tjhammer845@gmail.com>
2020-10-14 16:32:43 -0400

    slides

---
+++ build/precache-manifest.7982bd5a7f67fdaaff95feb74e14ad15.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f0cf4f898b6e4c5dafd2319a1a05ec",
+    "url": "/react-portfolio-tjh/index.html"
+  },
+  {
+    "revision": "a42398151a97d7d41e5c",
+    "url": "/react-portfolio-tjh/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "30f1d56c312cbe962551",
+    "url": "/react-portfolio-tjh/static/css/main.9cbeb59c.chunk.css"
+  },
+  {
+    "revision": "a42398151a97d7d41e5c",
+    "url": "/react-portfolio-tjh/static/js/2.20f64577.chunk.js"
+  },
+  {
+    "revision": "368f0afb5072fb72f7f16c7a2cf45ac2",
+    "url": "/react-portfolio-tjh/static/js/2.20f64577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f1d56c312cbe962551",
+    "url": "/react-portfolio-tjh/static/js/main.5bdad229.chunk.js"
+  },
+  {
+    "revision": "5231f34c82abe0eaebe3",
+    "url": "/react-portfolio-tjh/static/js/runtime-main.4730b83a.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/react-portfolio-tjh/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "461088f65916a75989727e8f7c012fac",
+    "url": "/react-portfolio-tjh/static/media/evverest.461088f6.png"
+  },
+  {
+    "revision": "9b21af92c0285765c25a756840e04b4a",
+    "url": "/react-portfolio-tjh/static/media/logo.9b21af92.png"
+  },
+  {
+    "revision": "c7d614e39a33a5273c84b655bbde7e21",
+    "url": "/react-portfolio-tjh/static/media/youtube.c7d614e3.png"
+  }
+]);
