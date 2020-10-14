a02b333636d60a87d45279fa6b7eb504fb60d6fa
Thomas Hammer <tjhammer845@gmail.com>
2020-10-14 15:26:37 -0400

    logo size

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio-tjh/precache-manifest.1f9467d87a7199a8af32bf6ce1ebffd2.js"
+  "/react-portfolio-tjh/precache-manifest.b12c3d85695dba49e74470b75b532503.js"
 );
 
 self.addEventListener('message', (event) => {
