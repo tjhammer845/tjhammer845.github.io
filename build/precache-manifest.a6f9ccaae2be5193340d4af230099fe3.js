d909231047bf2b2ee466f21f13188c2e6e2c3c55
Thomas Hammer <tjhammer845@gmail.com>
2020-10-14 21:35:29 -0400

    react parser to add font awesome icons in state

---
+++ build/precache-manifest.a6f9ccaae2be5193340d4af230099fe3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586a9cfe56f1be45c8be31bbd99140cf",
+    "url": "/react-portfolio-tjh/index.html"
+  },
+  {
+    "revision": "7c3fb0dde40c9d00c360",
+    "url": "/react-portfolio-tjh/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "41487080d5b674049798",
+    "url": "/react-portfolio-tjh/static/css/main.e5894796.chunk.css"
+  },
+  {
+    "revision": "7c3fb0dde40c9d00c360",
+    "url": "/react-portfolio-tjh/static/js/2.d626f3e5.chunk.js"
+  },
+  {
+    "revision": "368f0afb5072fb72f7f16c7a2cf45ac2",
+    "url": "/react-portfolio-tjh/static/js/2.d626f3e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41487080d5b674049798",
+    "url": "/react-portfolio-tjh/static/js/main.0318df70.chunk.js"
+  },
+  {
+    "revision": "5231f34c82abe0eaebe3",
+    "url": "/react-portfolio-tjh/static/js/runtime-main.4730b83a.js"
+  },
+  {
+    "revision": "461088f65916a75989727e8f7c012fac",
+    "url": "/react-portfolio-tjh/static/media/evverest.461088f6.png"
+  },
+  {
+    "revision": "9b21af92c0285765c25a756840e04b4a",
+    "url": "/react-portfolio-tjh/static/media/logo.9b21af92.png"
+  },
+  {
+    "revision": "c7d614e39a33a5273c84b655bbde7e21",
+    "url": "/react-portfolio-tjh/static/media/partnersNewHaven.c7d614e3.png"
+  },
+  {
+    "revision": "4d0beac1e459024bff3ee68066b62665",
+    "url": "/react-portfolio-tjh/static/media/yaleUI.4d0beac1.png"
+  }
+]);
