35dae86c173fa63c941f0ec3f1120358b97ff0be
Thomas Hammer <tjhammer845@gmail.com>
2020-10-18 20:00:03 -0400

    stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,22 +15,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Footer() {
 
     return (
-        <footer className="mt-5 fixed-bottom bg-red">
+        <footer className="mt-5 fixed-bottom bg-red pl-3 pr-3 border-top">
             <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
+                <Row className="justify-content-md-between p-3">
                     <Col className="p-0" md={3} sm={12}>
                         <ExternalLink target="_blank" className="pr-3" href="https://github.com/tjhammer845/"><FontAwesomeIcon className="display-5" icon={faGithubAlt} /></ExternalLink>
                         <ExternalLink target="_blank" className="pr-3" href="https://www.linkedin.com/in/thomasjhammer/"><FontAwesomeIcon className="display-5" icon={faLinkedinIn} /></ExternalLink>
                         <ExternalLink target="_blank" className="pr-3" href="https://twitter.com/tjhammer845"><FontAwesomeIcon className="display-5" icon={faTwitter} /></ExternalLink>
                         <Link target="_blank" className="p-0" to="./pdf/TH-CV.pdf"><FontAwesomeIcon className="display-5" icon={faFileDownload} /></Link>
                     </Col>
-                    <Col className="p-0 d-flex justify-content-end" md={3} sm={12}>
+                    <Col className="p-0 d-flex justify-content-center justify-content-md-end" md={3} sm={12}>
                         <font className="text-white">&copy; 2020 Tom Hammer. All Rights Reserved.</font>
                     </Col>
                 </Row>
             </Container>
         </footer>
     );
+
 }
 
 export default Footer;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -18,11 +18,11 @@ function Hero(props) {
                 <Row className="justify-content-center pt-5">
                     <Col md={10} sm={12} className="text-center">
                         {props.title && <h2 className="display-1 font-weight-bolder mt-5">{props.title}</h2>}
-                        {props.subTitle && <h3 className="display-2 font-weight-light mt-4">{props.subTitle}</h3>}
+                        {props.subTitle && <h3 className="display-2 font-weight-light mt-2">{props.subTitle}</h3>}
                     </Col>
                 </Row>
                 <Row className="justify-content-center py-4">
-                    <Col md={6} sm={12} className="text-center">
+                    <Col md={5} sm={12} className="text-center">
                         {props.text && <h4 className="lead font-weight-light">{props.text}</h4>}
                         <h2 className="display-4 mt-4">
                             <FontAwesomeIcon className="p-2 color-red" icon={faPencilRuler} />
