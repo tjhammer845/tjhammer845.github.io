7d43a77b58249b9ca8fa66cc47fcd6a83aad4a9f
Thomas Hammer <tjhammer845@gmail.com>
2020-10-19 10:41:15 -0400

    Updates

--- precache-manifest.1ecc89230563e06970f106ecf3a2ea3a.js
+++ precache-manifest.1ecc89230563e06970f106ecf3a2ea3a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "355ec7889233c8ae18a032327c0e938e",
+    "revision": "a663c17b417866c696637485c8aedc86",
     "url": "/react-portfolio-tjh/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio-tjh/static/css/2.1bbf9bdc.chunk.css"
   },
   {
-    "revision": "f1eaf0a78a5591c73c2c",
-    "url": "/react-portfolio-tjh/static/css/main.19a65a31.chunk.css"
+    "revision": "9a12f2b83849ee35b7fa",
+    "url": "/react-portfolio-tjh/static/css/main.07cf3936.chunk.css"
   },
   {
     "revision": "8f8761479e9e49d1b89c",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio-tjh/static/js/2.04d48f11.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f1eaf0a78a5591c73c2c",
+    "revision": "9a12f2b83849ee35b7fa",
     "url": "/react-portfolio-tjh/static/js/main.0a606e83.chunk.js"
   },
   {
