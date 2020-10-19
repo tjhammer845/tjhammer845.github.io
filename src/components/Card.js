455938653ab61dc5d8985946609bb86656804d20
Thomas Hammer <tjhammer845@gmail.com>
2020-10-19 00:19:36 -0400

    stuff

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import CardInfo from '../components/CardInfo';
-import ReactCSSTransitionGroup from 'react-transition-group'; // ES6
+import { CSSTransitionGroup } from 'react-transition-group' // ES6
 
 function Card(props) {
     return (
-        <ReactCSSTransitionGroup
+        <CSSTransitionGroup
             transitionName="example"
             transitionEnterTimeout={500}
             transitionLeaveTimeout={300}>
@@ -12,7 +12,7 @@ function Card(props) {
                 <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
                 {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
             </div>
-        </ReactCSSTransitionGroup>
+        </CSSTransitionGroup>
     );
 }
 
