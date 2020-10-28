93ea0691e766e7f5a0e549965305e7e5c94ffaa8
Thomas Hammer <tjhammer845@gmail.com>
2020-10-27 21:45:23 -0400

    test

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,7 +14,7 @@ const responsive = {
     superLargeDesktop: {
         // the naming can be any, depends on you.
         breakpoint: { max: 4000, min: 3000 },
-        items: 5
+        items: 4
     },
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
@@ -105,29 +105,11 @@ class Gallery extends React.Component {
         return (
             <Container fluid={true}>
                 <Row className='justify-content-around text-center'>
-                    <Carousel responsive={responsive}
-                        swipeable={false}
-                        draggable={false}
-                        showDots={true}
-                        responsive={responsive}
-                        ssr={true} // means to render carousel on server-side.
-                        infinite={true}
-                        autoPlay={this.props.deviceType !== "mobile" ? true : false}
-                        autoPlaySpeed={1000}
-                        keyBoardControl={true}
-                        customTransition="all .5"
-                        transitionDuration={500}
-                        containerClass="carousel-container"
-                        removeArrowOnDeviceType={["tablet", "mobile"]}
-                        deviceType={this.props.deviceType}
-                        dotListClass="custom-dot-list-style"
-                        itemClass="carousel-item-padding-40-px"
-                    >
+                    <Carousel responsive={responsive}>
                         <div>Item 1</div>
                         <div>Item 2</div>
                         <div>Item 3</div>
                         <div>Item 4</div>
-                        {/* {this.makeItems(this.state.items)} */}
                     </Carousel>
                 </Row>
             </Container>
