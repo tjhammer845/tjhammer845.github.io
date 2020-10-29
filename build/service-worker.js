3eea1ad1790159df4a4a50f5a19321497dc7c624
Thomas Hammer <tjhammer845@gmail.com>
2020-10-29 10:07:31 -0400

    bg changes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio-tjh/precache-manifest.f4c3e4db557b4125ed72deaaebd16f84.js"
+  "/react-portfolio-tjh/precache-manifest.b5ce132062a59fa4fd979d64c19feaec.js"
 );
 
 self.addEventListener('message', (event) => {
