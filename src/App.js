92c4ae20b634328f3ed2c1851c964069a148e20e
Thomas Hammer <tjhammer845@gmail.com>
2020-10-29 19:29:32 -0400

    new 1

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
         logo:
             { imgSrc: logoImg, title: this.siteTitle },
         headerLinks: [
-            { title: 'Home', path: '/tjhammer845.github.io' },
+            { title: 'Home', path: '/' },
             { title: 'About', path: '/about' },
             { title: 'Workflow', path: '/workflow' },
             { title: 'Contact', path: '/contact' }
@@ -58,7 +58,7 @@ class App extends React.Component {
             <Router>
                 <Container className='main-container' fluid={true}>
                     <Sticky {...this.state} />
-                    <Route path='/tjhammer845.github.io' exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+                    <Route path='/' exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
                     <Route path='/about' render={() => <AboutPage about={this.state.about} title={this.state.about.title} />} />
                     <Route path='/workflow' render={() => <WorkflowPage title={this.state.workflow.title} />} />
                     <Route path='/contact' render={() => <ContactPage title={this.state.contact.title} />} />
