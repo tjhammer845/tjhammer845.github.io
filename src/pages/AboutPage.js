6336f0c141d4a802828e685626b5d95988c75fe8
Thomas Hammer <tjhammer845@gmail.com>
2020-10-30 11:08:51 -0400

    test

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,7 +10,7 @@ function AboutPage(props) {
             <Content>
                 <div className='d-flex flex-column-reverse flex-xl-row justify-content-xl-between'>
                     <div>
-                        <p className='lead'>I am a front-end engineer and digital creator focused on writing clean and readable code. With an education and background in digital design, I understand the entire process of creating quality web applications from conception to completion. In the past, I’ve worked on a variety of web projects and gained invaluable experiences through daily operations within Information Technology and Creative.</p>
+                        <p className='lead'>I am a regular guy focused on making things pretty and writing clean, readable code. With an education and background in digital design, I understand the entire process of creating quality web applications from conception to completion. In the past, I’ve worked on a variety of web projects and gained invaluable experiences through daily operations within Information Technology and Creative.</p>
                         <p>Outside of the office, I tend to be the fun-loving adventurous type, always seeking new sites and taking on new types of challenges. I like to spend my time cooking, gardening, drawing, weightlifting, running, and mountain biking. I love to stay busy and on the move, both in and out of the workplace.</p>
                     </div>
                     <div className='d-flex justify-content-center justify-content-xl-end'>
