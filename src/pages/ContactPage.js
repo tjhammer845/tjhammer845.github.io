8dfc50c55e3a892d15463709be5173356b416918
Thomas Hammer <tjhammer845@gmail.com>
2020-11-02 21:17:18 -0500

    test

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -16,7 +16,7 @@ class ContactPage extends React.Component {
         e.preventDefault();
         axios({
             method: "POST",
-            url: "tom.thecaffeineteam.com/contact/send",
+            url: "tom.thecaffeineteam.com/send",
             data: this.state
         }).then((response) => {
             if (response.data.status === 'success') {
