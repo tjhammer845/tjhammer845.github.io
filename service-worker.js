9716f325dfd670e911fbe120898d66f3a9daa217
Thomas Hammer <tjhammer845@gmail.com>
2020-11-13 13:17:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.687e29396f94ddb1dc9ffd0a32792da7.js"
+  "/precache-manifest.d85f23f11bbd95bfbf53b8e0652e49c5.js"
 );
 
 self.addEventListener('message', (event) => {
