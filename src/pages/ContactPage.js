0fc2dd2f2da45458bedc3f475fe5f88eeaa31c80
Thomas Hammer <tjhammer845@gmail.com>
2020-12-16 10:21:35 -0500

    update resume

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,70 +1,36 @@
 import React, { useState } from 'react';
-import { Axios, db } from '../firebase/firebaseConfig';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 
 const ContactPage = (props) => {
-    const [formData, setFormData] = useState({})
-
-    const updateInput = e => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        })
-    }
-    const handleSubmit = event => {
-        event.preventDefault()
-        sendEmail()
-        setFormData({
-            name: '',
-            email: '',
-            message: '',
-        })
-    }
-    const sendEmail = () => {
-        Axios.post(
-            'https://us-central1-portfolio-contact-21e8f.cloudfunctions.net/submit',
-            formData
-        )
-            .then(res => {
-                db.collection('emails').add({
-                    name: formData.name,
-                    email: formData.email,
-                    message: formData.message,
-                    time: new Date(),
-                })
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    onChange={updateInput}
-                    value={formData.name || ''}
-                />
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    onChange={updateInput}
-                    value={formData.email || ''}
-                />
-                <textarea
-                    type="text"
-                    name="message"
-                    placeholder="Message"
-                    onChange={updateInput}
-                    value={formData.message || ''}
-                ></textarea>
-                <button type="submit">Submit</button>
-            </form>
-        </>
+        <div>
+            <Hero title={props.title} />
+            <Content>
+                <form id='contact-form' className='form'>
+                    <div className='form-group'>
+                        <label htmlFor='contactName'>Name</label>
+                        <input type='text' className='form-control' id='contactName'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='contactEmail'>Email</label>
+                        <input type='email' className='form-control' id='contactEmail'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='contactMessage'>Message</label>
+                        <textarea className='form-control' rows='5' id='contactMessage'
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <button type='submit' className='btn btn-primary'>Submit</button>
+                    </div>
+                </form>
+            </Content>
+        </div>
     )
 }
 
@@ -75,34 +41,3 @@ export default ContactPage
 
 
 
-// import React, { useState } from 'react';
-// import Hero from '../components/Hero';
-// import Content from '../components/Content';
-// import { db } from '../firebase';
-
-//         <div>
-//             <Hero title={props.title} />
-//             <Content>
-//                 <form id='contact-form' className='form' onSubmit={handleSubmit}>
-//                     <div className='form-group'>
-//                         <label htmlFor='contactName'>Name</label>
-//                         <input type='text' className='form-control' id='contactName'
-//                             value={name} onChange={(e) => setName(e.target.value)} />
-//                     </div>
-//                     <div className='form-group'>
-//                         <label htmlFor='contactEmail'>Email</label>
-//                         <input type='email' className='form-control' id='contactEmail'
-//                             value={email} onChange={(e) => setEmail(e.target.value)} />
-//                     </div>
-//                     <div className='form-group'>
-//                         <label htmlFor='contactMessage'>Message</label>
-//                         <textarea className='form-control' rows='5' id='contactMessage'
-//                             value={message} onChange={(e) => setMessage(e.target.value)} />
-//                     </div>
-//                     <div className='form-group'>
-//                         <button type='submit' className='btn btn-primary'
-//                             style={{ background: loader ? "#ccc" : "rgb(2,2,110)" }}>Submit</button>
-//                     </div>
-//                 </form>
-//             </Content>
-//         </div>
